f6d4db4f2aed801ed8a05d429e6acd02338ff422
JavBobby <bahadir.guler@hotmail.com>
2023-03-28 14:09:47 -0400

    deneme

--- src/test/java/com/library/pages/BorrowedBooksPage.java
+++ src/test/java/com/library/pages/BorrowedBooksPage.java
@@ -1,6 +1,8 @@
 package com.library.pages;
 
+import com.library.utility.Driver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
@@ -10,4 +12,27 @@ public class BorrowedBooksPage extends BasePage{
 
     @FindBy(xpath = "//tbody//td[2]")
     public List<WebElement> allBorrowedBooksName;
+
+
+
+    @FindBy(xpath = "//tbody//td[.='NOT RETURNED ']/preceding-sibling::td[4]")
+    public List<WebElement> notReturnedBooks;
+
+
+    @FindBy(xpath = "//a[@class='btn btn-primary btn-sm ']")
+    public List<WebElement> returnBookButton;
+
+
+
+    public void returnBooks(){
+        Actions actions = new Actions(Driver.getDriver());
+        for (WebElement each : new BorrowedBooksPage().returnBookButton) {
+            actions.moveToElement(each);
+            each.click();
+        }
+    }
+
+
+
+
 }
--- src/test/resources/library.sql
+++ src/test/resources/library.sql
@@ -1,9 +1,11 @@
-
 -- US 01
-select count(id) from users; --
+select count(id)
+from users;
+--
 -- 1855
 
-select count(distinct id) from users;
+select count(distinct id)
+from users;
 -- 1855
 
 
@@ -11,10 +13,66 @@ select count(distinct id) from users;
 
 
 -- US 02
-select * from users;
+select *
+from users;
 
+select *
+from users
+where email = 'student5@library';
 
 
 -- US 03
-select count(*) from book_borrow
-where is_returned=0;
+select count(*)
+from book_borrow
+where is_returned = 0;
+
+-- US 07 Bobby
+select *
+from books;
+
+select *
+from books
+where id = '8924';
+
+select *
+from book_borrow;
+select *
+from book_borrow
+where book_id = '8924';
+
+select count(*)
+from books B
+         join book_borrow bb on B.id = bb.book_id;
+
+select *
+from book_borrow bb
+         inner join books b on bb.book_id = b.id
+         inner join users u on bb.user_id = u.id
+where u.id = '5757'
+  and bb.is_returned = 0
+  and b.id = '8924';
+
+select u.id
+from book_borrow bb
+         join books b on bb.book_id = b.id
+         join users u on bb.user_id = u.id
+where bb.is_returned = 0
+  and b.id = '8924';
+
+select *
+from books B
+         join book_borrow bb on B.id = bb.book_id
+where bb.book_id = '8924'
+  and bb.is_returned < 1;
+
+select full_name, b.name, bb.borrowed_date
+from users u
+         inner join book_borrow bb on u.id = bb.user_id
+         inner join books b on bb.book_id = b.id
+where full_name = 'Test Student 5'
+  and name = 'Bobby Book'
+order by 3 desc;
+
+select count(*) from book_borrow where is_returned=0;
+
+select name from book_categories;
